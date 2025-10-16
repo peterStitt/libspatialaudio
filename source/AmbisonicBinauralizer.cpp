peterStitt/libspatialaudio

commit 1dc37eb11980fb8b95cdd6eb3bfd20f05ec65d58

Improve AmbisonicDecoder and make it more in line with best practice.
Compensate AmbiX inputs in decoding matrix rather than weighting the input signal.

Update preset decoders.

Add Octave scripts for generating header files of the presets.

Add a test that can be used to verify the output of the preset decoders against data in Octave.

source/AmbisonicDecoder.cpp
@@ -38,8 +38,11 @@ bool CAmbisonicDecoder::Configure(unsigned nOrder, bool b3D, unsigned nBlockSize
     bool success = CAmbisonicBase::Configure(nOrder, b3D, 0);
     if(!success)
         return false;
+
     // Set up the ambisonic shelf filters
-    shelfFilters.Configure(nOrder, b3D, nBlockSize, sampleRate);
+    m_shelfFilters.Configure(nOrder, b3D, nBlockSize, sampleRate);
+
+    m_pBFSrcTmp.Configure(nOrder, b3D, nBlockSize);
 
     SpeakerSetUp(nSpeakerSetUp, nSpeakers);
     Refresh();
@@ -51,7 +54,8 @@ void CAmbisonicDecoder::Reset()
 {
     for(unsigned niSpeaker = 0; niSpeaker < m_nSpeakers; niSpeaker++)
         m_pAmbSpeakers[niSpeaker].Reset();
-    shelfFilters.Reset();
+    m_shelfFilters.Reset();
+    m_pBFSrcTmp.Reset();
 }
 
 void CAmbisonicDecoder::Refresh()
@@ -64,18 +68,30 @@ void CAmbisonicDecoder::Refresh()
     // Load the preset
     LoadDecoderPreset();
 
-    shelfFilters.Refresh();
+    m_shelfFilters.Refresh();
+    m_pBFSrcTmp.Refresh();
 }
 
 void CAmbisonicDecoder::Process(CBFormat* pBFSrc, unsigned nSamples, float** ppfDst)
 {
     // If a preset is not loaded then use optimisation shelf filters
     if (!m_bPresetLoaded)
-        shelfFilters.Process(pBFSrc, nSamples);
-
-    for(unsigned niSpeaker = 0; niSpeaker < m_nSpeakers; niSpeaker++)
     {
-        m_pAmbSpeakers[niSpeaker].Process(pBFSrc, nSamples, ppfDst[niSpeaker]);
+        // Process a copy of the input to avoid overwriting it
+        m_pBFSrcTmp = *pBFSrc;
+        m_shelfFilters.Process(&m_pBFSrcTmp, nSamples);
+
+        for (unsigned niSpeaker = 0; niSpeaker < m_nSpeakers; niSpeaker++)
+        {
+            m_pAmbSpeakers[niSpeaker].Process(&m_pBFSrcTmp, nSamples, ppfDst[niSpeaker]);
+        }
+    }
+    else
+    {
+        for (unsigned niSpeaker = 0; niSpeaker < m_nSpeakers; niSpeaker++)
+        {
+            m_pAmbSpeakers[niSpeaker].Process(pBFSrc, nSamples, ppfDst[niSpeaker]);
+        }
     }
 }
 
@@ -502,6 +518,19 @@ void CAmbisonicDecoder::SpeakerSetUp(Amblib_SpeakerSetUps nSpeakerSetUp, unsigne
         m_pAmbSpeakers[0].SetPosition(polPosition);
         break;
     };
+
+    if (m_b3D)
+    {
+        for (unsigned iSpk = 0; iSpk < m_nSpeakers; ++iSpk)
+            for (unsigned i = 0; i < m_nOrder; ++i)
+                m_pAmbSpeakers[iSpk].SetOrderWeight(i, 2.f * (float)i + 1.f);
+    }
+    else
+    {
+        for (unsigned iSpk = 0; iSpk < m_nSpeakers; ++iSpk)
+            for (unsigned i = 0; i < m_nOrder; ++i)
+                m_pAmbSpeakers[iSpk].SetOrderWeight(i, 2.f);
+    }
 
     fSpeakerGain = 1.f / sqrtf((float)m_nSpeakers);
     for(niSpeaker = 0; niSpeaker < m_nSpeakers; niSpeaker++)

include/AmbisonicDecoder.h
@@ -105,20 +105,20 @@ public:
      */
     PolarPoint GetPosition(unsigned nSpeaker);
 
-    /** Sets the weight [0,1] for the spherical harmonics of the given order,
+    /** Sets the weight for the spherical harmonics of the given order,
      *  at the given speaker.
      * @param nSpeaker  Index of the speaker to adjust.
      * @param nOrder    Order of the components to be weighted.
-     * @param fWeight   Weight [0,1] to be applied.
+     * @param fWeight   Weight to be applied.
      * @see GetOrderWeight
      */
     void SetOrderWeight(unsigned nSpeaker, unsigned nOrder, float fWeight);
 
-    /** Returns the weight [0,1] for the spherical harmonics of the given order,
+    /** Returns the weight for the spherical harmonics of the given order,
      *  at the given speaker.
      * @param nSpeaker  Speaker index
      * @param nOrder    Order of the components to get the weight for.
-     * @return          Weight [0,1] applied to the specified speaker and components.
+     * @return          Weight applied to the specified speaker and components.
      */
     float GetOrderWeight(unsigned nSpeaker, unsigned nOrder);
 
@@ -160,7 +160,9 @@ protected:
     bool m_bPresetLoaded;
 
 private:
-    CAmbisonicOptimFilters shelfFilters;
+    CAmbisonicOptimFilters m_shelfFilters;
+    // A temp version of the input when optimisation filtering is applied to avoid overwriting the input
+    CBFormat m_pBFSrcTmp;
 };
 
 #endif // _AMBISONIC_DECODER_H

include/AmbisonicDecoderPresets.h
@@ -14,68 +14,68 @@
 // Decoder coefficients for Ambisonics to stereo. Useful for conversion to 2-channels when not using headphone.
 const float decoder_coefficient_stereo[][16] =
 {
-{0.5f, 0.5f / 3.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f},
-{0.5f, -0.5f / 3.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f}
+{0.5f, 0.5f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f},
+{0.5f, -0.5f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f}
 };
 
-// First order decoder for 5.1 loudspeaker array
+// First order AmbiX decoder for 5.1 loudspeaker array provided by Bruce Wiggins
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_first_5_1[][4] = { {0.300520f, 0.135000f, 0.000000f, 0.120000f},
-{0.300520f, -0.135000f, 0.000000f, 0.120000f},
-{0.332340f, 0.138333f, 0.000000f, -0.110000f},
-{0.332340f, -0.138333f, 0.000000f, -0.110000f},
-{0.141421f, 0.000000f, 0.000000f, 0.053333f},
+const float decoder_coefficient_first_5_1[][4] = { {0.300520f, 0.405000f, 0.000000f, 0.360000f},
+{0.300520f, -0.405000f, 0.000000f, 0.360000f},
+{0.332340f, 0.415000f, 0.000000f, -0.330000f},
+{0.332340f, -0.415000f, 0.000000f, -0.330000f},
+{0.141421f, 0.000000f, 0.000000f, 0.160000f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f} };
 
-// Second order decoder for 5.1 loudspeaker array
+// Second order AmbiX decoder for 5.1 loudspeaker array provided by Bruce Wiggins
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_second_5_1[][9] = { {0.286378f, 0.103333f, -0.000000f, 0.106667f, 0.028868f, 0.000000f, 0.000000f, 0.000000f, 0.019630f},
-{0.286378f, -0.103333f, -0.000000f, 0.106667f, -0.028868f, 0.000000f, 0.000000f, -0.000000f, 0.019630f},
-{0.449013f, 0.093333f, -0.000000f, -0.111667f, 0.018475f, -0.000000f, -0.000000f, 0.000000f, -0.018475f},
-{0.449013f, -0.093333f, -0.000000f, -0.111667f, -0.018475f, -0.000000f, -0.000000f, 0.000000f, -0.018475f},
-{0.060104f, 0.000000f, 0.000000f, 0.013333f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.010392f},
+const float decoder_coefficient_second_5_1[][9] = { {0.286378f, 0.310000f, -0.000000f, 0.320000f, 0.144338f, 0.000000f, 0.000000f, 0.000000f, 0.098150f},
+{0.286378f, -0.310000f, -0.000000f, 0.320000f, -0.144338f, 0.000000f, 0.000000f, -0.000000f, 0.098150f},
+{0.449013f, 0.280000f, -0.000000f, -0.335000f, 0.092376f, -0.000000f, -0.000000f, 0.000000f, -0.092376f},
+{0.449013f, -0.280000f, -0.000000f, -0.335000f, -0.092376f, -0.000000f, -0.000000f, 0.000000f, -0.092376f},
+{0.060104f, 0.000000f, 0.000000f, 0.040000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.051962f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f} };
 
-// Third order decoder for 5.1 loudspeaker array
+// Third order AmbiX decoder for 5.1 loudspeaker array provided by Bruce Wiggins
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_third_5_1[][16] = { {0.219203f, 0.095000f, 0.000000f, 0.103333f, 0.042724f, 0.000000f, 0.000000f, 0.000000f, 0.001155f, 0.010842f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.004518f},
-{0.219203f, -0.095000f, 0.000000f, 0.103333f, -0.042724f, 0.000000f, 0.000000f, 0.000000f, 0.001155f, -0.010842f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.004518f},
-{0.417193f, 0.128333f, 0.000000f, -0.111667f, 0.004619f, 0.000000f, 0.000000f, 0.000000f, -0.005774f, -0.011746f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.004518f},
-{0.417193f, -0.128333f, 0.000000f, -0.111667f, -0.004619f, 0.000000f, 0.000000f, 0.000000f, -0.005774f, 0.011746f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.004518f},
-{0.095459f, 0.000000f, 0.000000f, 0.088333f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.049652f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.018974f},
+const float decoder_coefficient_third_5_1[][16] = { {0.219203f, 0.285000f, 0.000000f, 0.310000f, 0.213620f, 0.000000f, 0.000000f, 0.000000f, 0.005774f, 0.075895f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.031623f},
+{0.219203f, -0.285000f, 0.000000f, 0.310000f, -0.213620f, 0.000000f, 0.000000f, 0.000000f, 0.005774f, -0.075895f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.031623f},
+{0.417193f, 0.385000f, 0.000000f, -0.335000f, 0.023094f, 0.000000f, 0.000000f, 0.000000f, -0.028868f, -0.082219f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.031623f},
+{0.417193f, -0.385000f, 0.000000f, -0.335000f, -0.023094f, 0.000000f, 0.000000f, 0.000000f, -0.028868f, 0.082219f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.031623f},
+{0.095459f, 0.000000f, 0.000000f, 0.265000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.248261f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.132816f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f} };
 
 
 // 7.1 Decoders
-// First order decoder for 7.1 loudspeaker array
+// First order AmbiX decoder for 7.1 loudspeaker array
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_first_7_1[][4] = { {0.303082f, 0.095958f, 0.000000f, 0.114243f},
-{0.303082f, -0.095958f, 0.000000f, 0.114243f},
-{0.300098f, 0.124767f, 0.000000f, -0.017447f},
-{0.300098f, -0.124767f, 0.000000f, -0.017447f},
-{0.259458f, 0.053266f, 0.000000f, -0.117329f},
-{0.259458f, -0.053266f, 0.000000f, -0.117329f},
-{0.066262f, 0.000000f, 0.000000f, 0.031737f},
+const float decoder_coefficient_first_7_1[][4] = { {0.303082f, 0.287875f, 0.000000f, 0.342728f},
+{0.303082f, -0.287875f, 0.000000f, 0.342728f},
+{0.300098f, 0.374300f, 0.000000f, -0.052342f},
+{0.300098f, -0.374300f, 0.000000f, -0.052342f},
+{0.259458f, 0.159799f, 0.000000f, -0.351986f},
+{0.259458f, -0.159799f, 0.000000f, -0.351986f},
+{0.066262f, 0.000000f, 0.000000f, 0.095211f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f} };
 
-// Second order decoder for 7.1 loudspeaker array
+// Second order AmbiX decoder for 7.1 loudspeaker array
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_second_7_1[][9] = { {0.268964f, 0.090325f, 0.000000f, 0.111024f, 0.044867f, 0.000000f, 0.000000f, 0.000000f, 0.015736f},
-{0.268964f, -0.090325f, 0.000000f, 0.111024f, -0.044867f, -0.000000f, 0.000000f, 0.000000f, 0.015736f},
-{0.229483f, 0.136694f, 0.000000f, -0.018120f, -0.020953f, 0.000000f, 0.000000f, 0.000000f, -0.049001f},
-{0.229483f, -0.136694f, 0.000000f, -0.018120f, 0.020953f, -0.000000f, 0.000000f, 0.000000f, -0.049001f},
-{0.216456f, 0.042012f, 0.000000f, -0.116220f, -0.038878f, 0.000000f, 0.000000f, 0.000000f, 0.032005f},
-{0.216456f, -0.042012f, 0.000000f, -0.116220f, 0.038878f, -0.000000f, 0.000000f, 0.000000f, 0.032005f},
-{0.058222f, 0.000000f, 0.000000f, 0.048933f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.025293f},
+const float decoder_coefficient_second_7_1[][9] = { {0.268964f, 0.270975f, 0.000000f, 0.333072f, 0.224335f, 0.000000f, 0.000000f, 0.000000f, 0.078680f},
+{0.268964f, -0.270975f, 0.000000f, 0.333072f, -0.224335f, -0.000000f, 0.000000f, 0.000000f, 0.078680f},
+{0.229483f, 0.410083f, 0.000000f, -0.054360f, -0.104766f, 0.000000f, 0.000000f, 0.000000f, -0.245007f},
+{0.229483f, -0.410083f, 0.000000f, -0.054360f, 0.104766f, -0.000000f, 0.000000f, 0.000000f, -0.245007f},
+{0.216456f, 0.126035f, 0.000000f, -0.348660f, -0.194391f, 0.000000f, 0.000000f, 0.000000f, 0.160026f},
+{0.216456f, -0.126035f, 0.000000f, -0.348660f, 0.194391f, -0.000000f, 0.000000f, 0.000000f, 0.160026f},
+{0.058222f, 0.000000f, 0.000000f, 0.146799f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.126464f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f} };
 
-// Third order decoder for 7.1 loudspeaker array
+// Third order AmbiX decoder for 7.1 loudspeaker array
 // Do not activate psychoacoustic optimisation filters
-const float decoder_coefficient_third_7_1[][16] = { {0.238475f, 0.085873f, 0.000000f, 0.114877f, 0.054573f, 0.000000f, 0.000000f, 0.000000f, 0.015163f, 0.006254f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.006185f},
-{0.238475f, -0.085873f, 0.000000f, 0.114877f, -0.054573f, -0.000000f, 0.000000f, 0.000000f, 0.015163f, -0.006254f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.006185f},
-{0.214882f, 0.124042f, 0.000000f, -0.017580f, -0.018064f, 0.000000f, 0.000000f, 0.000000f, -0.060255f, -0.011908f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.008159f},
-{0.214882f, -0.124042f, 0.000000f, -0.017580f, 0.018064f, -0.000000f, 0.000000f, 0.000000f, -0.060255f, 0.011908f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.008159f},
-{0.197904f, 0.043357f, 0.000000f, -0.115673f, -0.048364f, 0.000000f, 0.000000f, 0.000000f, 0.034129f, 0.017198f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.001868f},
-{0.197904f, -0.043357f, 0.000000f, -0.115673f, 0.048364f, -0.000000f, 0.000000f, 0.000000f, 0.034129f, -0.017198f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.001868f},
-{0.077144f, 0.000000f, 0.000000f, 0.045620f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.030548f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.025329f},
+const float decoder_coefficient_third_7_1[][16] = { {0.238475f, 0.257618f, 0.000000f, 0.344630f, 0.272865f, 0.000000f, 0.000000f, 0.000000f, 0.075817f, 0.043776f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.043293f},
+{0.238475f, -0.257618f, 0.000000f, 0.344630f, -0.272865f, -0.000000f, 0.000000f, 0.000000f, 0.075817f, -0.043776f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.043293f},
+{0.214882f, 0.372126f, 0.000000f, -0.052741f, -0.090319f, 0.000000f, 0.000000f, 0.000000f, -0.301275f, -0.083357f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.057111f},
+{0.214882f, -0.372126f, 0.000000f, -0.052741f, 0.090319f, -0.000000f, 0.000000f, 0.000000f, -0.301275f, 0.083357f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.057111f},
+{0.197904f, 0.130070f, 0.000000f, -0.347020f, -0.241820f, 0.000000f, 0.000000f, 0.000000f, 0.170646f, 0.120386f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.013078f},
+{0.197904f, -0.130070f, 0.000000f, -0.347020f, 0.241820f, -0.000000f, 0.000000f, 0.000000f, 0.170646f, -0.120386f, -0.000000f, -0.000000f, 0.000000f, 0.000000f, 0.000000f, -0.013078f},
+{0.077144f, 0.000000f, 0.000000f, 0.136861f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.152741f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.177300f},
 {0.500000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f, 0.000000f} };

include/AmbisonicSource.h
@@ -58,13 +58,13 @@ public:
      */
     virtual PolarPoint GetPosition();
 
-    /** Sets the weight [0,1] for the spherical harmonics of the given order.
+    /** Sets the weight for the spherical harmonics of the given order.
      * @param nOrder    The order to set the weights for
      * @param fWeight   Weight to be applied to all coefficients of the specified order.
      */
     virtual void SetOrderWeight(unsigned nOrder, float fWeight);
 
-    /** Sets the weight [0,1] for the spherical harmonics of all orders.
+    /** Sets the weight for the spherical harmonics of all orders.
      * @param fWeight Weight to be applied to all coefficients.
      */
     virtual void SetOrderWeightAll(float fWeight);
@@ -76,7 +76,7 @@ public:
      */
     virtual void SetCoefficient(unsigned nChannel, float fCoeff);
 
-    /** Gets the weight [0,1] for the spherical harmonics of the given order.
+    /** Gets the weight for the spherical harmonics of the given order.
      * @param nOrder    Order of coefficients
      * @return          Weight applied to coefficients of the specified order.
      */

include/Tests.h
@@ -596,3 +596,55 @@ void testRotation()
 	}
 
 }
+
+/** Test the loudspeaker decoder presets
+*/
+void testDecoderPresets()
+{
+	unsigned nSamples = 1;
+	unsigned order = 1;
+	bool b3D = true;
+	unsigned sampleRate = 48000;
+	auto layout = Amblib_SpeakerSetUps::kAmblib_71;
+
+	CAmbisonicEncoder ambiEnc;
+	bool success = ambiEnc.Configure(order, b3D, 0);
+	assert(success);
+
+	CAmbisonicDecoder ambiDec;
+	success = ambiDec.Configure(order, b3D, nSamples, sampleRate, layout);
+	assert(success);
+
+	unsigned nLdspk = ambiDec.GetSpeakerCount();
+
+	CBFormat inputSignal;
+	inputSignal.Configure(order, b3D, nSamples);
+	inputSignal.Reset();
+	std::vector<float> impulse(nSamples, 0.f);
+	impulse[0] = 1.f;
+	std::vector<std::vector<float>> output(inputSignal.GetChannelCount(), std::vector<float>(nSamples, 0.f));
+
+	float** ldspkOut = new float* [nLdspk];
+	for (int iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+		ldspkOut[iLdspk] = new float[nSamples];
+
+	for (float az = 0.f; az < 360.f; az += 1.f)
+	{
+		ambiEnc.SetPosition({ DegreesToRadians(az),0.f,1. }, 0.f);
+		ambiEnc.Refresh();
+		ambiEnc.Process(impulse.data(), nSamples, &inputSignal);
+
+		ambiDec.Process(&inputSignal, nSamples, ldspkOut);
+
+		for (unsigned iSamp = 0; iSamp < nSamples; ++iSamp)
+		{
+			for (unsigned iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+				std::cout << ldspkOut[iLdspk][iSamp] << ", ";
+			std::cout << std::endl;
+		}
+	}
+
+	for (unsigned iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+		delete ldspkOut[iLdspk];
+	delete[] ldspkOut;
+}

octave/IrregularLayoutDecoders/DecodersToHeaders.m
@@ -1,0 +1,26 @@
+% Output decoders to c++ headers for inclusion
+clear
+close all
+
+load('decodingMatrices_5p0_AmbiX.mat');
+load('decodingMatrices_7p0_AmbiX.mat');
+
+% Add LFE as the last channel to decoders
+D_1_50(end+1,1) = 0.5;
+D_2_50(end+1,1) = 0.5;
+D_3_50(end+1,1) = 0.5;
+D_1_70(end+1,1) = 0.5;
+D_2_70(end+1,1) = 0.5;
+D_3_70(end+1,1) = 0.5;
+
+fID = fopen('decoderMatrices.h','w');
+
+matrixToHeader(fID,D_1_50.','decoder_coefficient_first_5_1');
+matrixToHeader(fID,D_2_50.','decoder_coefficient_second_5_1');
+matrixToHeader(fID,D_3_50.','decoder_coefficient_third_5_1');
+matrixToHeader(fID,D_1_70.','decoder_coefficient_first_7_1');
+matrixToHeader(fID,D_2_70.','decoder_coefficient_second_7_1');
+matrixToHeader(fID,D_3_70.','decoder_coefficient_third_7_1');
+
+fclose(fID);
+

octave/IrregularLayoutDecoders/SaveWigginsDecoders.m
@@ -1,0 +1,95 @@
+% Decoders for 5.1 layouts supplied by Bruce Wiggins:
+##Copyright 2017 Dr Bruce Wiggins
+##bruce.wiggins@gmail.com
+##http://www.brucewiggins.co.uk
+##
+##Permission is hereby granted, free of charge, to any person obtaining a copy of this
+##software and associated documentation files (the "Software"), to deal in the Software
+##without restriction, including without limitation the rights to use, copy, modify,
+##merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+##permit persons to whom the Software is furnished to do so, subject to the following
+##conditions:
+##The above copyright notice and this permission notice shall be included in all copies
+##or substantial portions of the Software.
+##
+##THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+##INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+##PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+##LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+##TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+##OR OTHER DEALINGS IN THE SOFTWARE.
+
+% This script takes FuMa convention 5.1 decoders and converts them to AmbiX decoders
+% Author: Peter Stitt
+% Data: May 2024
+
+clear
+close all
+
+% order 1 FuMa decoder
+D_1 = [0.4250	0.3600 	0.4050	0.0;
+    0.4250	0.3600 	-0.4050	0.0;
+    0.4700	-0.3300	0.4150	0.0;
+    0.4700	-0.3300	-0.4150	0.0;
+    0.200  	0.1600 	0.0000	0.0];
+
+% order 1 FuMa-to-AmbiX
+D_1_50(:,1) = D_1(:,1)/sqrt(2);
+D_1_50(:,2) = D_1(:,3);
+D_1_50(:,3) = D_1(:,4);
+D_1_50(:,4) = D_1(:,2);
+
+D_1_51 = D_1_50;
+D_1_51(end+1,1) = 0.5;
+
+% order 2 FuMa decoder
+D_2 = [0.4050  0.32000  0.31000  -0.00000  0.00000  0.00000   0.00000   0.08500   0.12500;
+    0.4050  0.32000 -0.31000  -0.00000  0.00000 -0.00000   0.00000   0.08500  -0.12500;
+    0.6350 -0.33500  0.28000  -0.00000 -0.00000  0.00000  -0.00000  -0.08000   0.08000;
+    0.6350 -0.33500 -0.28000  -0.00000 -0.00000  0.00000  -0.00000  -0.08000  -0.08000;
+    0.0850  0.04000  0.0       0.00000  0.0      0.00000   0.0       0.04500   0.0];
+
+% order 2 FuMa-to-AmbiX
+D_2_50(:,1) = D_2(:,1)/sqrt(2);
+D_2_50(:,2) = D_2(:,3);
+D_2_50(:,3) = D_2(:,4);
+D_2_50(:,4) = D_2(:,2);
+D_2_50(:,5) = D_2(:,9)/(sqrt(3)/2);
+D_2_50(:,6) = D_2(:,7)/(sqrt(3)/2);
+D_2_50(:,7) = D_2(:,5);
+D_2_50(:,8) = D_2(:,6)/(sqrt(3)/2);
+D_2_50(:,9) = D_2(:,8)/(sqrt(3)/2);
+
+D_2_51 = D_2_50;
+D_2_51(end+1,1) = 0.5;
+
+% Order 3 FuMa decoder
+D_3 = [0.3100 0.2850 0 0.3100 0.1850 0 0 0 0.0050 0.0600 0 0 0 0 0 -0.0250;
+    0.3100 -0.2850 0 0.3100 -0.1850 0 0 0 0.0050 -0.0600 0 0 0 0 0 -0.0250;
+    0.5900 0.3850 0 -0.3350 0.0200 0 0 0 -0.0250 -0.0650 0 0 0 0 0 0.0250;
+    0.5900 -0.3850 0 -0.3350 -0.0200 0 0 0 -0.0250 0.0650 0 0 0 0 0 0.0250;
+    0.1350 0 0 0.2650 0 0 0 0 0.2150 0 0 0 0 0 0 0.1050    ];
+
+% Order 3 FuMa-to-AmbiX
+D_3_50(:,1) = D_3(:,1)/sqrt(2);
+D_3_50(:,2) = D_3(:,2);
+D_3_50(:,3) = D_3(:,3);
+D_3_50(:,4) = D_3(:,4);
+D_3_50(:,5) = D_3(:,5)/(sqrt(3)/2);
+D_3_50(:,6) = D_3(:,6)/(sqrt(3)/2);
+D_3_50(:,7) = D_3(:,7);
+D_3_50(:,8) = D_3(:,8)/(sqrt(3)/2);
+D_3_50(:,9) = D_3(:,9)/(sqrt(3)/2);
+D_3_50(:,10) = D_3(:,10)/sqrt(5/8);
+D_3_50(:,11) = D_3(:,11)/(sqrt(5)/3);
+D_3_50(:,12) = D_3(:,12)/sqrt(32/45);
+D_3_50(:,13) = D_3(:,13);
+D_3_50(:,14) = D_3(:,14)/sqrt(32/45);
+D_3_50(:,15) = D_3(:,15)/(sqrt(5)/3);
+D_3_50(:,16) = D_3(:,16)/sqrt(5/8);
+
+D_3_51 = D_3_50;
+D_3_51(end+1,1) = 0.5;
+
+save('decodingMatrices_5p0_AmbiX.mat','D_1_50','D_2_50','D_3_50')
+save('decodingMatrices_5p1_AmbiX.mat','D_1_51','D_2_51','D_3_51')

octave/IrregularLayoutDecoders/decodingMatrices_5p0_AmbiX.mat
@@ -1,0 +1,34 @@
+# Created by Octave 8.4.0, Wed May 08 10:56:53 2024 GMT <unknown@PeterLaptop>
+# name: D_1_50
+# type: matrix
+# rows: 5
+# columns: 4
+ 0.30052038200428266 0.40500000000000003 0 0.35999999999999999
+ 0.30052038200428266 -0.40500000000000003 0 0.35999999999999999
+ 0.33234018715767727 0.41499999999999998 0 -0.33000000000000002
+ 0.33234018715767727 -0.41499999999999998 0 -0.33000000000000002
+ 0.1414213562373095 0 0 0.16
+
+
+# name: D_2_50
+# type: matrix
+# rows: 5
+# columns: 9
+ 0.28637824638055176 0.31 -0 0.32000000000000001 0.14433756729740646 0 0 0 0.09814954576223639
+ 0.28637824638055176 -0.31 -0 0.32000000000000001 -0.14433756729740646 0 0 -0 0.09814954576223639
+ 0.44901280605345767 0.28000000000000003 -0 -0.33500000000000002 0.092376043070340128 -0 -0 0 -0.092376043070340128
+ 0.44901280605345767 -0.28000000000000003 -0 -0.33500000000000002 -0.092376043070340128 -0 -0 0 -0.092376043070340128
+ 0.060104076400856542 0 0 0.040000000000000001 0 0 0 0 0.051961524227066319
+
+
+# name: D_3_50
+# type: matrix
+# rows: 5
+# columns: 16
+ 0.21920310216782971 0.28499999999999998 0 0.31 0.21361959960016155 0 0 0 0.005773502691896258 0.075894663844041102 0 0 0 0 0 -0.031622776601683791
+ 0.21920310216782971 -0.28499999999999998 0 0.31 -0.21361959960016155 0 0 0 0.005773502691896258 -0.075894663844041102 0 0 0 0 0 -0.031622776601683791
+ 0.417193000900063 0.38500000000000001 0 -0.33500000000000002 0.023094010767585032 0 0 0 -0.028867513459481291 -0.082219219164377855 0 0 0 0 0 0.031622776601683791
+ 0.417193000900063 -0.38500000000000001 0 -0.33500000000000002 -0.023094010767585032 0 0 0 -0.028867513459481291 0.082219219164377855 0 0 0 0 0 0.031622776601683791
+ 0.095459415460183911 0 0 0.26500000000000001 0 0 0 0 0.2482606157515391 0 0 0 0 0 0 0.13281566172707193
+
+

commit 7ff0b7cc6346e036e61dbafdf56bbaada43e725a

Fix bug with AmbisonicBinauralizer that was introduced in commit 1dc37eb.
Before that commit the decoder coefficients in AmbisonicDecoder assumed N3D normalisation and correction for SN3D normalisation (used by the lib) was done at decoding.
1dc37eb moved the N3D->SN3D correction to the decoder coefficients. However, the old correction was left in AmbisonicBinauralizer, leading to double "correction".

This commit also fixes a bug where the correction gain was not applied to the highest order channels.

source/AmbisonicBinauralizer.cpp
@@ -112,9 +112,7 @@ namespace spaudio {
                     return false;
 
                 //Scale the HRTFs by the coefficient of the current channel/component
-                // The spherical harmonic coefficients are multiplied by (2*order + 1) to provide the correct decoder
-                // for SN3D normalised Ambisonic inputs.
-                float fCoefficient = m_AmbDecoder.GetCoefficient(niSpeaker, niChannel) * (2.f * floorf(sqrtf((float)niChannel)) + 1.f);
+                float fCoefficient = m_AmbDecoder.GetCoefficient(niSpeaker, niChannel);
                 for (niTap = 0; niTap < m_nTaps; niTap++)
                 {
                     pfHRTF[0][niTap] *= fCoefficient;

source/AmbisonicDecoder.cpp
@@ -745,14 +745,14 @@ namespace spaudio {
             else
             {
                 for (unsigned iSpk = 0; iSpk < m_nSpeakers; ++iSpk)
-                    for (unsigned i = 0; i < m_nOrder; ++i)
+                    for (unsigned i = 0; i <= m_nOrder; ++i)
                         m_pAmbSpeakers[iSpk].SetOrderWeight(i, 2.f * (float)i + 1.f);
             }
         }
         else
         {
             for (unsigned iSpk = 0; iSpk < m_nSpeakers; ++iSpk)
-                for (unsigned i = 0; i < m_nOrder; ++i)
+                for (unsigned i = 0; i <= m_nOrder; ++i)
                     m_pAmbSpeakers[iSpk].SetOrderWeight(i, 2.f);
         }
     }